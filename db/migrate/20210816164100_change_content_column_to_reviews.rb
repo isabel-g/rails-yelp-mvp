ab00c61c65374e63a14d8eaccc4b896a04c7d81e
isabel garcia <ing.cg9@gmail.com>
2021-08-16 13:16:21 -0400

    ejercicio2

---
+++ db/migrate/20210816164100_change_content_column_to_reviews.rb
@@ -0,0 +1,5 @@
+class ChangeContentColumnToReviews < ActiveRecord::Migration[6.0]
+  def change
+    change_column :reviews, :content, :text
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", rating: 5 }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", rating: 4 }
+pizza_n =  { name: "Pizza n", address: "calle norte", rating: 3 }
+pizza_s =  { name: "Pizza s", address: "calle sur", rating: 2 }
+pizza_m =  { name: "Pizza m", address: "calle m", rating: 4 }
+[ dishoom, pizza_east, pizza_n, pizza_s, pizza_m ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
